abf065acb67b87236283e67b9c1db41054f62a4f
Ahmad Saropi <ahmad@gmail.com>
2024-06-04 23:16:56 +0700

    membuat navbar

--- js/script.js
+++ js/script.js
@@ -25,23 +25,25 @@ replaceName();
 
 function validateForm() {
     const name = document.forms["message-form"]["full-name"].value;
+    const email = document.forms["message-form"]["email"].value;
     const birthDate = document.forms["message-form"]["birth-date"].value;
     const gender = document.forms["message-form"]["gender"].value;
     const messages = document.forms["message-form"]["messages"].value;
 
-    if (name =="" || birthDate == "" || gender == "" || messages == "") {
+    if (name =="" || email =="" || birthDate == "" || gender == "" || messages == "") {
         alert("Tidak boleh ada yang kosong");
         return false;
     }
 
-    setSenderUi(name, birthDate, gender, messages);
+    setSenderUi(name, email, birthDate, gender, messages);
 
     return false;
 
 }
 
-function setSenderUi(name, birthDate, gender, messages) {
+function setSenderUi(name, email, birthDate, gender, messages) {
     document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-email").innerHTML = email;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
     document.getElementById("sender-gender").innerHTML = gender;
     document.getElementById("sender-messages").innerHTML = messages;
