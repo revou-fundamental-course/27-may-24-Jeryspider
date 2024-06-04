b52eb7c953c7fe2a28a8fd8f7b097760f782ca64
Ahmad Saropi <ahmad@gmail.com>
2024-06-04 19:04:47 +0700

    membuat section our facilitation dan message us

--- js/script.js
+++ js/script.js
@@ -12,3 +12,37 @@ document.addEventListener('click', function (e) {
         navbarNav.classList.remove('active');
     }
 });
+
+
+// doom name
+function replaceName() {
+    let name = prompt("Siapakah nama anda?", "");
+    document.getElementById("name-hero").innerHTML = name
+}
+
+replaceName();
+
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name =="" || birthDate == "" || gender == "" || messages == "") {
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
+
+    setSenderUi(name, birthDate, gender, messages);
+
+    return false;
+
+}
+
+function setSenderUi(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
